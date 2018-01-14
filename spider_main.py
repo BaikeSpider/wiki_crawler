d2d72a894a0f34dad2007b20e63521f42d17e259
Seoul <alaxn@qq.com>
2018-01-14 18:28:03 +0000

    sports

--- spider_main.py
+++ spider_main.py
@@ -77,11 +77,11 @@ class SpiderMain(object):
 
 
 if __name__=='__main__':
-    root_url = ['https://zh.wikipedia.org/wiki/%E6%94%BF%E7%AD%96']
-    new_titles = ['政策']
-    parent = 'https://zh.wikipedia.org/wiki/%E6%94%BF%E7%AD%96'
-    parent_title = '政策'
-    parent_all = '政策'
+    root_url = ['https://zh.wikipedia.org/zh-cn/%E4%BD%93%E8%82%B2%E8%BF%90%E5%8A%A8']
+    new_titles = ['体育运动']
+    parent = 'Top'
+    parent_title = '体育运动'
+    parent_all = '体育运动'
     
     obj_spider = SpiderMain()
     obj_spider.craw(root_url, new_titles, parent, parent_title, parent_all)
